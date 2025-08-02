db7792984a530de1abca194613c9896b8870d4a1
Enyxe <yani.sid2007@gmail.com>
2025-08-03 00:54:28 +0200

    First commit

---
+++ hello_world.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+char *test = "";
+
+int setInfo(char *marque) {
+    return (0);
+}
+
+void main() {
+
+    setInfo("Audi");
+    printf("Hello, world!");
+}
---
+++ variable.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+
+const test = "zdqdzd";
+
+int main(void) {
+
+    int entier = 10; 
+    float flottant = 3.14;
+    char caractère = 'Audi';
+
+    printf("%d\n", entier);
+    return 0;
+}
+
+
+int main(void) {
+
+    int entier = 10; 
+    float flottant = 3.14;
+    char caractère = 'Audi';
+
+    
+    int *ptr = &entier;
+
+
+    printf("%d\n", entier);
+
+    printf("%f\n", entier);
+
+    printf("%c\n", entier);
+
+    printf("%s\n", entier);
+
+    printf("%p\n", entier);
+    return 0;
+}
+
+
+int main(void) {
+
+    int entier = 10; 
+    float flottant = 3.14;
+    char caractère = 'Audi';
+
+    printf("%d\n", entier);
+    return 0;
+}
+
+int main(void) {
+
+    int entier = 10; 
+    float flottant = 3.14;
+    char caractère = 'Audi';
+
+    printf("%d\n", entier);
+    return 0;
+}
